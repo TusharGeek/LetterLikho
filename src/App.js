c0b4e2d39aa44012ceab3c92af8b3d707a7d352f
TusharGeek <assignmentssimplify@gmail.com>
2023-01-21 22:29:29 +0530

    Footer Aadded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "../src/Components/Header";
 import Letter from "../src/Components/Letter";
-
+import Footer from "../src/Components/Footer"
 import './App.css';
 
 
@@ -10,12 +10,7 @@ function App() {
     <div className="App m-0 px-5 py-5 max-w-3xl m-auto ">
       <Header/>
       <Letter/>
-    
-   
-      <div className="flex gap-5">
-         {/* Buttons */}
-
-      </div>
+     <Footer/>
 
     </div>
   );
---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <div style={{marginTop:"4rem"}} className="text-center  ">
+        <p className="text-gray-400">Copyright © 2023</p>
+        <p className="text-gray-400">Developed By: Tushar, Ankit & Yadunandan</p>
+        </div>
+    )
+}
+
+export default Footer;
