01ff409905984c262144561469db5c7a8401d510
TusharGeek <assignmentssimplify@gmail.com>
2023-01-25 18:10:27 +0530

    OR ADDED

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,7 +12,7 @@ const HandleLettter1Click = () => {
 return (
     <div id="Home " className="h-screen  ">
          <div className="Grids my-20">
-          <div onClick={HandleLettter1Click} className="Grid bg-gray-100 rounded-sm p-5 h-28 shadow-md flex flex-col items-center justify-center">
+          <div onClick={HandleLettter1Click} className="Grid bg-gray-100 rounded-md p-5 h-28 shadow-md flex flex-col items-center justify-center">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
@@ -25,7 +25,7 @@ return (
           </div>
          
           </div>
-           <div className="Grid bg-gray-100 rounded-sm p-5 h-28 shadow-md flex flex-col items-center justify-center">
+           <div className="Grid bg-gray-100 rounded-md p-5 h-28 shadow-md flex flex-col items-center justify-center">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
@@ -38,7 +38,7 @@ return (
           </div>
          
           </div>
-          <div className="Grid bg-gray-100 rounded-sm p-5 h-28 shadow-md flex flex-col items-center justify-center">
+          <div className="Grid bg-gray-100 rounded-md p-5 h-28 shadow-md flex flex-col items-center justify-center">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
@@ -51,7 +51,7 @@ return (
           </div>
          
           </div>
-          <div className="Grid bg-gray-100 rounded-sm p-5 h-28 shadow-md flex flex-col items-center justify-center">
+          <div className="Grid bg-gray-100 rounded-md p-5 h-28 shadow-md flex flex-col items-center justify-center">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
@@ -64,8 +64,10 @@ return (
           </div>
          
           </div>
-         
-         <div style={{backgroundColor:"#ff7f7f",gridColumn:"1/3",gridRow:"4/3"}} className="Grid  rounded-sm shadow-md p-5 h-20 flex flex-row justify-center items-center">
+          <div style={{gridColumn:"1/3",gridRow:"4"}} className="Grid  p-5 h-20 flex flex-row justify-center items-center">
+        <p className="font-semibold text-center text-gray-600">- OR -</p> 
+         </div>
+         <div style={{backgroundColor:"#ff7f7f",gridColumn:"1/3",gridRow:"5"}} className="Grid  rounded-md shadow-md p-5 h-20 flex flex-row justify-center items-center">
         <p style={{color:"white"}} className="font-semibold text-center">AI Letter Generater</p> 
          </div>
          </div>
